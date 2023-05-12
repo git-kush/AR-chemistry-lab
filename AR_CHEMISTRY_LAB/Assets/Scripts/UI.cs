2b97eb039096c3af58a4481b631a4cc25f79b10b
lakshyajain2026 <lakshyajain2026@gmail.com>
2023-05-12 19:23:31 +0530

    UI

---
+++ AR_CHEMISTRY_LAB/Assets/Scripts/UI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btnstart : MonoBehaviour
+{
+   
+    public void ChangeScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void LoadNextScenefrbuild()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+
+     public void QuitGame()
+    {
+        Application.Quit();
+    }
